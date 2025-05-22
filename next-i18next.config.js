5d8d2d5cf334228d5e1cc34589d42784ec519753
Anthony Guido <guido.anthony@orange.fr>
2025-05-22 13:14:49 +0200

    chore: add explicit path translations

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+const path = require('path')
+
+const i18nextConfig = {
   i18n: {
     defaultLocale: 'en-US',
     locales: ['en-US', 'es-CO', 'fr-BE'],
   },
   reloadOnPrerender: process.env.NODE_ENV === 'development',
+  localePath: path.resolve('./public/locales'),
 }
+
+module.exports = i18nextConfig

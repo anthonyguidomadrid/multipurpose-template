ff05db4f256696e3eef9902b8eb410c8532daa1e
Anthony Guido <guido.anthony@orange.fr>
2025-05-22 13:32:23 +0200

    chore: add variable Google font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,17 +7,9 @@ export default function Document() {
         {/* Add global meta tags, fonts, or styles here */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
-          rel="stylesheet"
-        />
-        <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Mrs+Saint+Delafield&display=swap"
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap"
-          rel="stylesheet"
-        ></link>
       </Head>
       <body>
         <Main />

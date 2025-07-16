10aa1211589785030a83a0a47cf004b1346d9e16
Anthony Guido <guido.anthony@orange.fr>
2025-07-16 12:25:35 +0200

    fix: image helper

--- src/helpers/image.ts
+++ src/helpers/image.ts
@@ -3,8 +3,8 @@ import { Image } from '@/lib/types'
 export const getImageDetails = (image: Image) => {
   return {
     imageUrl: `https:${image.fields.file.url}`,
-    imageWidth: image.fields.file.details?.image.width,
-    imageHeight: image.fields.file.details?.image.height,
+    imageWidth: image.fields.file.details?.image?.width,
+    imageHeight: image.fields.file.details?.image?.height,
     imageDescription: image.fields.description,
   }
 }

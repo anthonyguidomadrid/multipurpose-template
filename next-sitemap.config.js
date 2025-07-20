c6b0bcb4a2e484580ff245954ba74ee7ffd5055d
Anthony Guido <guido.anthony@orange.fr>
2025-07-20 22:18:30 +0200

    fix: sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const { getSlugs } = require('./src/helpers/getSlugs')
 
-
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
   generateRobotsTxt: true,
@@ -15,9 +14,15 @@ module.exports = {
   additionalPaths: async () => {
     const events = await getSlugs('event')
     const services = await getSlugs('service')
+
+    const props = {
+      lastmod: new Date().toISOString(),
+      changefreq: 'weekly',
+      priority: 0.7,
+    }
     return [
-      ...events.map((e) => ({ loc: `/events/${e.slug}` })),
-      ...services.map((s) => ({ loc: `/services/${s.slug}` })),
+      ...events.map((e) => ({ loc: `/events/${e.slug}`, ...props })),
+      ...services.map((s) => ({ loc: `/services/${s.slug}`, ...props })),
     ]
   },
 }
